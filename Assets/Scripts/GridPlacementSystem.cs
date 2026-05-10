df00a4b590ee1ced4c1f6449bdc0c38abcededc3
aaabsa <111131968+aaabsa@users.noreply.github.com>
2026-05-10 03:45:40 -0400

    hell... im in hell I hate you codemonkey my mortal enemy

--- Assets/Scripts/GridPlacementSystem.cs
+++ Assets/Scripts/GridPlacementSystem.cs
@@ -1,39 +1,181 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
+using UnityEngine.InputSystem;
 
-public class GridPlacementSystem : MonoBehaviour
-{
+public class GridBuildingSystem3D : MonoBehaviour {
+
+    public static GridBuildingSystem3D Instance { get; private set; }
+
+    public event EventHandler OnSelectedChanged;
+    public event EventHandler OnObjectPlaced;
+
+
     private GridXZ<GridObject> grid;
+    [SerializeField] private List<PlacedObjectTypeSO> placedObjectTypeSOList = null;
+    private PlacedObjectTypeSO placedObjectTypeSO;
+    private PlacedObjectTypeSO.Dir dir;
 
-    private void Awake()
-    {
+    private void Awake() {
+        Instance = this;
+
         int gridWidth = 10;
         int gridHeight = 10;
         float cellSize = 10f;
-        grid = new GridXZ<GridObject>(gridWidth, gridHeight, cellSize, Vector3.zero, (GridXZ<GridObject> g, int x, int y) => new GridObject(g, x, y));
+        grid = new GridXZ<GridObject>(gridWidth, gridHeight, cellSize, new Vector3(0, 0, 0), (GridXZ<GridObject> g, int x, int y) => new GridObject(g, x, y));
+
+        placedObjectTypeSO = null;// placedObjectTypeSOList[0];
     }
 
+    public class GridObject {
 
-
-//THE OBJECT TO SPAWN
-    public class GridObject
-    {
-        private int x;
-        private int z;
         private GridXZ<GridObject> grid;
+        private int x;
+        private int y;
+        public PlacedObject placedObject;
 
-        public GridObject(GridXZ<GridObject> grid, int x, int z)
-        {
+        public GridObject(GridXZ<GridObject> grid, int x, int y) {
             this.grid = grid;
             this.x = x;
-            this.z = z;
-
+            this.y = y;
+            placedObject = null;
         }
 
-        public override string ToString()
-        {
-            return x + ", " + z;
+        public override string ToString() {
+            return x + ", " + y + "\n" + placedObject;
         }
 
+        public void SetPlacedObject(PlacedObject placedObject) {
+            this.placedObject = placedObject;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public void ClearPlacedObject() {
+            placedObject = null;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public PlacedObject GetPlacedObject() {
+            return placedObject;
+        }
+
+        public bool CanBuild() {
+            return placedObject == null;
+        }
 
     }
+
+    private void Update() {
+        if (Mouse.current.leftButton.wasPressedThisFrame && placedObjectTypeSO != null) {
+            
+            Debug.Log("helloooo");
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            grid.GetXZ(mousePosition, out int x, out int z);
+
+            Vector2Int placedObjectOrigin = new Vector2Int(x, z);
+            placedObjectOrigin = grid.ValidateGridPosition(placedObjectOrigin);
+
+            // Test Can Build
+            List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(placedObjectOrigin, dir);
+            bool canBuild = true;
+            foreach (Vector2Int gridPosition in gridPositionList) {
+                if (!grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
+                    canBuild = false;
+                    break;
+                }
+            }
+
+            if (canBuild) {
+                Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+                Vector3 placedObjectWorldPosition = grid.GetWorldPosition(placedObjectOrigin.x, placedObjectOrigin.y) + new Vector3(rotationOffset.x, 0, rotationOffset.y) * grid.GetCellSize();
+
+                PlacedObject placedObject = PlacedObject.Create(placedObjectWorldPosition, placedObjectOrigin, dir, placedObjectTypeSO);
+
+                foreach (Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
+                }
+
+                OnObjectPlaced?.Invoke(this, EventArgs.Empty);
+
+                //DeselectObjectType();
+            } else {
+                // Cannot build here
+                UtilsClass.CreateWorldTextPopup("Cannot Build Here!", mousePosition);
+            }
+        }
+
+        if (Keyboard.current.rKey.wasPressedThisFrame) {
+            dir = PlacedObjectTypeSO.GetNextDir(dir);
+        }
+
+        if (Keyboard.current.digit1Key.wasPressedThisFrame) { placedObjectTypeSO = placedObjectTypeSOList[0]; RefreshSelectedObjectType(); }
+        if (Keyboard.current.digit2Key.wasPressedThisFrame) { placedObjectTypeSO = placedObjectTypeSOList[1]; RefreshSelectedObjectType(); }
+        if (Keyboard.current.digit3Key.wasPressedThisFrame) { placedObjectTypeSO = placedObjectTypeSOList[2]; RefreshSelectedObjectType(); }
+        if (Keyboard.current.digit4Key.wasPressedThisFrame) { placedObjectTypeSO = placedObjectTypeSOList[3]; RefreshSelectedObjectType(); }
+        if (Keyboard.current.digit5Key.wasPressedThisFrame) { placedObjectTypeSO = placedObjectTypeSOList[4]; RefreshSelectedObjectType(); }
+        if (Keyboard.current.digit6Key.wasPressedThisFrame) { placedObjectTypeSO = placedObjectTypeSOList[5]; RefreshSelectedObjectType(); }
+
+        if (Keyboard.current.digit0Key.wasPressedThisFrame) { DeselectObjectType(); }
+
+
+        if (Mouse.current.rightButton.wasPressedThisFrame) {
+            Debug.Log("helloooo1");
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            if (grid.GetGridObject(mousePosition) != null) {
+                // Valid Grid Position
+                PlacedObject placedObject = grid.GetGridObject(mousePosition).GetPlacedObject();
+                if (placedObject != null) {
+                    // Demolish
+                    placedObject.DestroySelf();
+
+                    List<Vector2Int> gridPositionList = placedObject.GetGridPositionList();
+                    foreach (Vector2Int gridPosition in gridPositionList) {
+                        grid.GetGridObject(gridPosition.x, gridPosition.y).ClearPlacedObject();
+                    }
+                }
+            }
+        }
+    }
+
+    private void DeselectObjectType() {
+        placedObjectTypeSO = null; RefreshSelectedObjectType();
+    }
+
+    private void RefreshSelectedObjectType() {
+        OnSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    public Vector2Int GetGridPosition(Vector3 worldPosition) {
+        grid.GetXZ(worldPosition, out int x, out int z);
+        return new Vector2Int(x, z);
+    }
+
+    public Vector3 GetMouseWorldSnappedPosition() {
+        Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+        grid.GetXZ(mousePosition, out int x, out int z);
+
+        if (placedObjectTypeSO != null) {
+            Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, z) + new Vector3(rotationOffset.x, 0, rotationOffset.y) * grid.GetCellSize();
+            return placedObjectWorldPosition;
+        } else {
+            return mousePosition;
+        }
+    }
+
+    public Quaternion GetPlacedObjectRotation() {
+        if (placedObjectTypeSO != null) {
+            return Quaternion.Euler(0, placedObjectTypeSO.GetRotationAngle(dir), 0);
+        } else {
+            return Quaternion.identity;
+        }
+    }
+
+    public PlacedObjectTypeSO GetPlacedObjectTypeSO() {
+        return placedObjectTypeSO;
+    }
+
 }
---
+++ Assets/Scripts/Mouse3D.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mouse3D : MonoBehaviour {
+
+    public static Mouse3D Instance { get; private set; }
+
+    [SerializeField] private LayerMask mouseColliderLayerMask = new LayerMask();
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.value);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderLayerMask)) {
+            transform.position = raycastHit.point;
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition() => Instance.GetMouseWorldPosition_Instance();
+
+    private Vector3 GetMouseWorldPosition_Instance() {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.value);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderLayerMask)) {
+            return raycastHit.point;
+        } else {
+            return Vector3.zero;
+        }
+    }
+
+}
