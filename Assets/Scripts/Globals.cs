7a4cff22d563ba310cc67c84dc334f3b27272be1
aaabsa <111131968+aaabsa@users.noreply.github.com>
2026-05-17 06:26:12 -0400

    fixed everything just gotta make it look nice

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -11,7 +11,7 @@ public static class Globals
 
     public static int currentDay = 1;
 
-    public static List<int> inventory = new List<int>();
+    public static List<int> inventory = new List<int>() { 0, 1, 2, 3, 4, 5,6,7,8,9 };
     public static int inventoryIndex = 0;
 
     public static List<string> carTetrisSaveList =  new List<string>() { "", "", "", "", "", "" };
